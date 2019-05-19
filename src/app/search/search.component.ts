03297d5a3d545e29239b6abe64bebbd335ab6a2f
Spase Kocev <spase.kocev@gmail.com>
2019-05-19 18:59:33 +0200

    Change angular mat theme

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CatalogBook } from '../CatalogBook';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
 import { FormControl } from '@angular/forms';
