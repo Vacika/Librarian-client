fca19b3c0e27122b8b8a48bb7e80248744b858e5
vasko.jovanoski@sorsix.com <vasko.jovanoski@sorsix.com>
2019-05-19 18:44:20 +0200

    Fixed search alignment

---
+++ src/app/popular-books/popular-books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-books',
+  templateUrl: './popular-books.component.html',
+  styleUrls: ['./popular-books.component.css']
+})
+export class PopularBooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
