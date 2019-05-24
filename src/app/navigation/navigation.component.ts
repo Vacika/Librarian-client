d8f615a9fc32d24a6d176fed460afd501fd9973e
vasko.jovanoski@sorsix.com <vasko.jovanoski@sorsix.com>
2019-05-25 01:45:17 +0200

    Added signout functionality

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+    constructor(private authservice: AuthenticationService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    logout() {
+        this.authservice.logout()
+            .subscribe(_ => localStorage.removeItem('currentUser'));
+    }
 }
