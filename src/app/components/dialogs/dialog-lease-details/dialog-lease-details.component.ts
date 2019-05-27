b7fece6f611061de8e91ce763fd4f30dc88fcb15
Vacika <vacikabre@hotmail.com>
2019-05-27 18:47:04 +0200

    Fixed Button 'set as returned' showing to users too

--- src/app/components/dialogs/dialog-lease-details/dialog-lease-details.component.ts
+++ src/app/components/dialogs/dialog-lease-details/dialog-lease-details.component.ts
@@ -1,18 +1,23 @@
-import { Inject, Component } from '@angular/core';
+import { Inject, Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Lease } from '../../../domain/Lease';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
     selector: 'app-dialog-lease-details',
     templateUrl: './dialog-lease-details.component.html',
     styleUrls: ['./dialog-lease-details.component.css']
 })
-export class DialogLeaseDetailComponent {
-
+export class DialogLeaseDetailComponent implements OnInit {
+    userRole: string;
     constructor(
         public dialogRef: MatDialogRef<DialogLeaseDetailComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: Lease) { }
+        @Inject(MAT_DIALOG_DATA) public data: Lease,
+        private service: AuthenticationService) { }
 
+    ngOnInit() {
+        this.service.user.subscribe(user => this.userRole = user.authorities[0].authority)
+    }
     onNoClick(): void {
         this.dialogRef.close();
     }
